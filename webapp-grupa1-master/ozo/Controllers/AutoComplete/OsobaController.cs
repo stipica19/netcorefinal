763c0046a2465ef14e76f8d643302c8c568cc078
stipica19 <pipiklepic1@gmail.com>
2019-06-10 21:53:01 +0200

    Zadnje

---
+++ webapp-grupa1-master/ozo/Controllers/AutoComplete/OsobaController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ozo.Models;
+using Microsoft.Extensions.Options;
+
+
+namespace ozo.Controllers.AutoComplete
+{
+        [Route("autocomplete/[controller]")]
+        public class OsobaController : Controller
+        {
+            private readonly PI01Context _context;
+            private readonly AppSettings appData;
+
+            public OsobaController(PI01Context ctx, IOptions<AppSettings> options)
+            {
+             this._context = ctx;
+             appData = options.Value;
+        }
+
+        public IOptions<AppSettings> Options { get; }
+
+        [HttpGet]
+            public IEnumerable<IdLabel> Get(string term)
+            {
+                var query = _context.Vw_Osoba
+                                .Select(o => new IdLabel
+                                {
+                                    Id = o.ZanimanjeId,
+                                    Label = o.NazivZanimanja
+                                })
+                                .Where(l => l.Label.Contains(term));
+
+                var list = query.OrderBy(l => l.Label)
+                                .ThenBy(l => l.Id)
+                                .ToList();
+                return list;
+            }
+        }
+    }
+
